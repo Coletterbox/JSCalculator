06f82f7d9d728f29de3dc8f8e9f255acd25ee436
Colette <38261091+Coletterbox@users.noreply.github.com>
2019-06-30 17:04:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// object.onclick = function() {
+//   alert("I don't turn off...");
+// }
+
+// function offFunction() {
+//   // var elem = document.getElementById("offButton");
+//   // alert("I don't turn off...");
+//   // document.getElementById("total").style.color = #530d6d;
+//
+//   var elem = document.getElementById("total");
+//   elem.style.color = red;
+// }
+
+var offFunction = function() {
+  alert("I don't turn off.");
+}
+
+// offButton.addEventListener("click", offFunction());
+
+var placeholderColor = function() {
+  var placeholderZero = document.getElementById.("total");
+  
+}
+
+function calculate(result) {
+    form.total.value = form.total.value + result;
+}
